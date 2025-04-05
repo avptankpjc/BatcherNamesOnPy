6c112d8d7247e0509e71ea3e9013ac6d1384b128
AvellanedaPardo <avepardo7ytubero@gmail.com>
2025-04-05 17:26:08 -0500

    C01_3 Configure the Setup File For the  Exe

--- setup.py
+++ setup.py
@@ -12,11 +12,26 @@ from cx_Freeze import setup, Executable
 #For add Icon
 #executables = [Executable("tu_script.py", base=base, icon="icono.ico")]
 
+#sys.setrecursionlimit(2000)
+
 build_exe_options = {
-    "packages":["tkinter", "os"],
-    "excludes":[],
-     "include_files": [
-        ("assets/img/icon_logo/A00_1_Logo_BatcherName_256.ico")
+    "packages":["tkinter", "tkinterdnd2", "os"],
+    "excludes":[
+            "tensorflow",
+            "tensorflow_core",
+            "tensorflow.python",
+            "tensorflow_estimator",
+            "django",
+            "matplotlib",
+            "matplotlib_inline",
+            "jinja2"
+        ],
+    "include_files": [
+        ("assets/img/icon_logo/A00_1_Logo_BatcherName_256.ico"),
+        ("model", "model"),
+        ("view", "view"),
+        ("viewmod", "viewmod"),
+        ("utils", "utils")
     ]
     #"include_files":[
     #    (tcl_dll, "tcl86t.dll"),
@@ -32,7 +47,7 @@ if sys.platform == "win32":
 
 setup(
     name="BatcherNameApp",
-    version="1.1.2",
+    version="1.2.0",
     description="Applications for rename files by batch",
     author="Avptankpowerjc",
     options={"build_exe":build_exe_options},
